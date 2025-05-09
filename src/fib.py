3cf643ff7f46c241187629458c5f4971487998a5
victo <victormiguel.gf@uma.es>
2025-05-09 14:49:07 +0200

    feat(fib): added parallelization support

--- src/fib.py
+++ src/fib.py
@@ -2,44 +2,29 @@ import functools
 import argparse
 import numpy as np
 from numpy.linalg import matrix_power
+import textwrap
+import time
+import threading
+from multiprocessing import Process, Queue
 
-iterations = 0
-def fibonacci_naive(n: int) -> int:
-    global iterations
+from parallelization import parallel_computing_recursive, fibonacci_worker, fib_even, fib_odd
+
+
+def fibonacci_recursive(n: int, parallel :bool = False, depth :int = 0) -> int:
     if n < 0:
         raise ValueError("n must be grater than 0")
-    elif n==0:
-        return 0
-    elif n==1:
-        iterations+=1
-        return 1
+    if n < 2:
+        return n
+    if parallel and depth:
+        return parallel_computing_recursive(fibonacci_recursive, n, depth)
     else:
-        iterations+=1
-        return fibonacci_naive(n-1) + fibonacci_naive(n-2)
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
 
 
-
-def fibmat(n):
-    i = np.array([[0, 1], [1, 1]])
-    return np.matmul(matrix_power(i, n), np.array([1, 0]))[1]
-
-
-def fib(n):
-    if n%2 == 0:#even
-        return fib_even(n)
-    return fib_odd(n)
-
-def fib_odd(N):
-    n = int((N+1)/2)
-    Fn = fibmat(n) 
-    Fn1 = fibmat(n-1)
-    return Fn1**2 + Fn**2
-
-def fib_even(N):
-    n = int(N/2)
-    Fn = fibmat(n) 
-    Fn1 = fibmat(n-1)
-    return Fn * (2*Fn1 + Fn)
+def fib_matrix(n: int, parallel :bool = False, depth :int = 0):
+    if n%2 == 0:
+        return fib_even(n, parallel, depth)
+    return fib_odd(n, parallel, depth)
 
 
 def fibonacci_iterative(n: int) -> int:
@@ -57,23 +42,17 @@ def fibonacci_iterative(n: int) -> int:
     return n0
 
 
-def fibonacci_recursive(n: int) -> int:
-    if n < 0:
-        raise ValueError("n must be grater than 0")
-    if n < 2:
-        return n
-    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
-
-
 cache = {}
 
-def fibonacci_recursive_memoization(n: int) -> int:
+def fibonacci_recursive_memoization(n: int, parallel :bool = False, depth :int = 0) -> int:
     if n < 0:
         raise ValueError("n must be grater or equal to zero.")
     if n < 2:
         return n
     if n in cache:
         return cache[n]
+    if parallel and depth:
+        return parallel_computing_recursive(fibonacci_recursive_memoization, n, depth)
     
     nth = fibonacci_recursive_memoization(n - 1) + fibonacci_recursive_memoization(n - 2)
 
@@ -82,20 +61,34 @@ def fibonacci_recursive_memoization(n: int) -> int:
     return nth
 
 
+
 @functools.cache
-def fibonacci_recursive_cache(n: int) -> int:
+def fibonacci_recursive_cache(n: int, parallel :bool = False, depth :int = 0) -> int:
     if n < 0:
         raise ValueError("n must be grater or equal to zero.")
     if n < 2:
         return n
+    if parallel and depth:
+        return parallel_computing_recursive(fibonacci_recursive_cache, n, depth)
     return fibonacci_recursive_cache(n - 1) + fibonacci_recursive_cache(n - 2)
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("nth", type=int, help="n-th Fibonacci number")
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('nth', type=int, help='n-th fibonacci number')
+    parser.add_argument('-f', '--func', choices=['n','m','i','r','rm','rc'], help=textwrap.dedent('''\
+        the fibonacci function to choose:
+          n   - naive
+          m   - matrix exponentiation
+          i   - iterative
+          r   - recursive
+          rm  - recursive with memoization
+          rc  - recursive with cache
+        '''), default='n', metavar='FUNC')
+    parser.add_argument('-p','--parallel', help='use the function with parallelization', action="store_true")
     args = parser.parse_args()
 
-    result = fibonacci_recursive(args.nth)
-    print(result)
+
+    result = fibonacci_recursive_cache(args.nth)
