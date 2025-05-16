203e4ef11613176c3a372b11ba2c393f46e75b15
victo <victormiguel.gf@uma.es>
2025-05-16 19:47:23 +0200

    feat: added logs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import argparse
 import textwrap
+import logging
 from parser import parallel_choices, parallel_args, parallel_functions
 
 if __name__ == "__main__":
@@ -18,11 +19,21 @@ if __name__ == "__main__":
     parser.add_argument('-p','--parallel', help='use the function with parallelization', action="store_true")
     parser.add_argument('-d','--depth', type = int, help='how many cores to use in parallelization', default=2)
 
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
     args = parser.parse_args()
 
     for p in parallel_choices:
         if args.func == p:
+            logging.info(f"Calculating fibonacci number {args.nth} using {p} function...")
+            
+            if args.parallel:
+                logging.info(f"Using parallelization with depth {args.depth}...")
+                if args.depth < 1 or args.depth > 8:
+                    logging.warning(f"Depth can not be {args.depth}. It must be greater than 1 or less than 8. Using default value of 2.")
+                    args.depth = 2
+
             fib_function = parallel_functions[parallel_choices.index(p)]
             result = parallel_args(fib_function, args.nth, args.parallel, args.depth)
 
-    print(result) 
+    print(result)
